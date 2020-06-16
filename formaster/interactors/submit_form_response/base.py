e34095e38ff45464c9bef6b398152f879b7ba168
revanth <revanth.1518@gmail.com>
2020-06-16 10:15:51 +0530

    inheritance in interactors

---
+++ formaster/interactors/submit_form_response/base.py
@@ -0,0 +1,43 @@
+"""
+Created on 16/06/20
+
+@author: revanth
+"""
+from abc import abstractmethod
+
+
+class BaseSubmitFormResponseInteractor:
+
+    def __init__(self, storage: StorageInterface, question_id: int,
+                 form_id: int, user_id: int):
+        self.storage = storage
+        self.question_id = question_id
+        self.form_id = form_id
+        self.user_id = user_id
+
+    def submit_form_response_wrapper(self, presenter: PresenterInterface):
+        try:
+            user_response_id = self.submit_form_response(presenter)
+            return presenter.submit_form_response_return(user_response_id)
+        except FormDoesNotExist:
+            presenter.raise_form_does_not_exist_exception()
+        except QuestionDoesNotBelongToForm:
+            presenter.raise_question_does_not_belong_to_form_exception()
+        except InvalidUserResponseSubmit:
+            presenter.raise_invalid_user_response_submitted()
+
+    def submit_form_response(self, presenter):
+        self.storage.get_form(self.form_id)
+        self.storage.validate_question_id_with_form(
+            self.question_id, self.form_id)
+        self._validate_user_response()
+        user_response_id = self._create_user_response()
+        return user_response_id
+
+    @abstractmethod
+    def _validate_user_response(self):
+        pass
+
+    @abstractmethod
+    def _create_user_response(self) -> int:
+        pass
